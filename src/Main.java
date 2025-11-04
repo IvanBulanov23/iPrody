0a695618774c54ad77e4d1fc4fd709070e1e3398
Bulanov Ivan <vanya.bulanov.1996@mail.ru>
2025-11-04 23:01:44 +0300

    Add home work lesson 8

--- src/Main.java
+++ src/Main.java
@@ -5,60 +5,18 @@ import java.util.Random;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        Employee ivanov = new Employee("Иванов Иван Иванович", "fitter", "ivanov@mal.ru", "89184441379", 130d, 56);
+        Employee obezhev = new Employee("Объезжев Алексей Николаевич", "engineer", "obezhev@mal.ru", "89181245512", 150d, 45);
+        Employee zobnin = new Employee("Зобнин Дмитрий Анатольевич", "buyer", "zobnin@mal.ru", "89181442876", 140d, 32);
+        Employee titov = new Employee("Титов Матвей Владимирович", "director", "titov@mal.ru", "89181243667", 210d, 41);
+        Employee zamkovaya = new Employee("Замковая Мария Дмитриевна", "secretary", "zamkovaya@mal.ru", "89184841394", 120d, 25);
 
-        int[] array = {3, 5, 2, 1, 4, 5};
-        int[] array2 = {1, 5, 7};
-        arrayCopy(array, array2);
-        int[] array3 = {3, 6, 9, 10, 3, 5, 2, 4, 7};
-        sortingSheker(array3);
-        Car car = new Car();
-        car.start();
-        Employee employee = new Employee("ФИО", "email", "telephone", 19);
-
-    }
-
-    public static void arrayCopy (int[] array, int[] array2){ //O(n)
-        int [] result = new int[array.length + array2.length];
-        if (array2.length > 0) {       //1
-            for (int i = 0; i < array.length; i++){  //n
-                result[i] = array[i];
-            }
-            for (int i = 0; i < array2.length; i++){  //n
-            result[i+array.length] = array2[i];
+        Employee[] employees = new Employee[]{ivanov, obezhev, zobnin, titov, zamkovaya};
+        for (Employee emp : employees) {
+            if (emp.getAge() > 40) { // или if (emp.age > 40)
+                emp.print();
             }
         }
-        else result = array;
-        System.out.println(Arrays.toString(result));
-    }
 
-    public static void sortingSheker (int[] array){
-        int leftIndex = 0, righIndex = array.length - 1;
-        int hou;
-        do {
-            int tempRightIndex = righIndex;
-            for (int i = leftIndex; i < tempRightIndex ; i++) {
-                if (array[i] > array[i+1]){
-                hou = array[i+1];
-                array[i+1] = array[i];
-                array[i] = hou;
-                righIndex = i;
-                }
-            }
-            if (tempRightIndex == righIndex){
-                break;
-            }
-            int tempLeftIndex = leftIndex;
-            for (int i = righIndex; i >= tempLeftIndex ; i--){
-                if (array[i] > array[i+1]){
-                    hou = array[i+1];
-                    array[i+1] = array[i];
-                    array[i] = hou;
-                    leftIndex = i;
-                }
-            }
-        }
-        while (leftIndex < righIndex);
-        System.out.println(Arrays.toString(array));
     }
-
 }
