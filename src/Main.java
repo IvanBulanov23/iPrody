7dd69ebd93a9059077dba36b0cdb5fb2a6f0db0c
Bulanov Ivan <vanya.bulanov.1996@mail.ru>
2025-12-07 21:14:00 +0300

    add lesson 17

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,61 @@
-import iprody.coffeeshot.CoffeeOrderBoard;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        CoffeeOrderBoard coffeeOrderBoard = new CoffeeOrderBoard();
-        coffeeOrderBoard.add("Ivan");
-        coffeeOrderBoard.add("Nikolay");
-        coffeeOrderBoard.add("Sergey");
-        coffeeOrderBoard.add("Lola");
-        coffeeOrderBoard.add("Zina");
-        coffeeOrderBoard.add("Launda");
+        Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(Arrays.toString(array));
+        swappingPlaces(array);
+        System.out.println(Arrays.toString(array));
 
-        CoffeeOrderBoard coffeeOrderBoard1 = new CoffeeOrderBoard();
-        coffeeOrderBoard1.add("Ivan");
-        coffeeOrderBoard1.add("Nikolay");
-        coffeeOrderBoard1.add("Sergey");
-        coffeeOrderBoard1.add("Lola");
+        String[] strings = {"A", "B", "C", "D", "E"};
+        System.out.println(Arrays.toString(strings));
+        swappingPlaces(strings);
+        System.out.println(Arrays.toString(strings));
 
-        coffeeOrderBoard.draw();
-        coffeeOrderBoard1.draw();
+        List<String> stringList = arraysToList(strings);
+        System.out.println(stringList);
+        stringList.add("F");
+        System.out.println(stringList);
+        stringList.add(0, "G");
+        System.out.println(stringList);
 
-        coffeeOrderBoard.deliver();
 
-        coffeeOrderBoard.deliver(4);
+        System.out.println("--- Создание ящиков ---");
+        Box<Apple> appleBox1 = new Box<>();
+        Box<Apple> appleBox2 = new Box<>();
+        Box<Orange> orangeBox1 = new Box<>();
+        Box<Orange> orangeBox2 = new Box<>();
+        Box<Fruit> mixedFruitBox = new Box<>();
+        appleBox1.add(new Apple(), 3);
+        orangeBox1.add(new Orange());
+        orangeBox1.add(new Orange());
 
-        coffeeOrderBoard.draw();
+        System.out.println(appleBox1.getWeight());
+        System.out.println(orangeBox1.getWeight());
+        System.out.println(appleBox1.compare(orangeBox1));
+        appleBox1.transfer(appleBox2);
+        appleBox1.print();
+        appleBox2.print();
 
-        coffeeOrderBoard.deliver(8);
 
+    }
+
+    public static <T> void swappingPlaces(T[] array) {
+        if (array == null) {
+            return;
+        }
+        for (int i = 0; i < array.length - 1; i += 2) {
+            T temp;
+            temp = array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = temp;
+        }
+    }
+
+    public static <T> List<T> arraysToList(T[] arrays) {
+        return new ArrayList<>(Arrays.asList(arrays));
     }
 
 }
