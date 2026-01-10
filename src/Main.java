57da91acfc7f6a3694022056295d3cf44a12ec0c
Bulanov Ivan <vanya.bulanov.1996@mail.ru>
2026-01-10 20:41:07 +0300

    add lesson 23

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ import java.io.IOException;
 import java.nio.file.Paths;
 
 public class Main {
-    public static void main(String[] args) {
-        ValueCalculator calculator = new ValueCalculator(1000000);
-        calculator.doCalc();
-
+    public static void main(String[] args) throws Exception {
+        TestRunner.start(MathUtilsTest.class);
     }
-
 }
+
